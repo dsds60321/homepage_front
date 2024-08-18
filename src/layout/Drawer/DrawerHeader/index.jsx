29500981bf33f8778a571426c4f498a716bd5c6a
dsds60321 <dsds60321@gmail.com>
2024-08-18 22:31:31 +0900

    refactor

--- src/layout/Drawer/DrawerHeader/index.jsx
+++ src/layout/Drawer/DrawerHeader/index.jsx
@@ -1,22 +1,32 @@
 import PropTypes from 'prop-types';
 
 // material-ui
-import { useTheme } from '@mui/material/styles';
+import { styled, useTheme } from '@mui/material/styles';
 
 // project import
 import DrawerHeaderStyled from './DrawerHeaderStyled';
-// import Logo from '@components/logo';
+import home from '@/assets/images/home.png';
 
 // ==============================|| DRAWER HEADER ||============================== //
 
-export default function DrawerHeader({ open }) {
-  const theme = useTheme();
+const BannerContainer = styled('div')(({ theme }) => ({
+    cursor: 'pointer',
+    width: '80%',
+    height: '100px', // 배너 높이 설정
+    backgroundImage: `url(${home})`, // 배너 이미지 URL
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    borderRadius: '8px', // 모서리 둥글게
+}));
 
-  return (
-    <DrawerHeaderStyled theme={theme} open={!!open}>
-      {/*<Logo isIcon={!open} sx={{ width: open ? 'auto' : 35, height: 35 }} />*/}
-    </DrawerHeaderStyled>
-  );
+export default function DrawerHeader({ open }) {
+    const theme = useTheme();
+
+    return (
+        <DrawerHeaderStyled theme={theme} open={!!open}>
+            <BannerContainer />
+        </DrawerHeaderStyled>
+    );
 }
 
 DrawerHeader.propTypes = { open: PropTypes.bool };
--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -1,8 +1,5 @@
 // project import
 import dashboard from './dashboard';
-import pages from './page';
-import utilities from './utilities';
-import support from './support';
 import physical from '@/menu-items/physical.jsx';
 
 // ==============================|| MENU ITEMS ||============================== //
