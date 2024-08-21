69e06ad08ee1d8ab236a54a3c29107abe21a459e
dsds60321 <dsds60321@gmail.com>
2024-08-21 22:01:18 +0900

    fix: 탭 수정

--- src/layout/Header/HeaderContent/index.jsx
+++ src/layout/Header/HeaderContent/index.jsx
@@ -16,27 +16,27 @@ import { GithubOutlined } from '@ant-design/icons';
 // ==============================|| HEADER - CONTENT ||============================== //
 
 export default function HeaderContent() {
-  const downLG = useMediaQuery((theme) => theme.breakpoints.down('lg'));
+    const downLG = useMediaQuery((theme) => theme.breakpoints.down('lg'));
 
-  return (
-    <>
-      {!downLG && <Search />}
-      {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
-      <IconButton
-        component={Link}
-        href="https://github.com/codedthemes/mantis-free-react-admin-template"
-        target="_blank"
-        disableRipple
-        color="secondary"
-        title="Download Free Version"
-        sx={{ color: 'text.primary', bgcolor: 'grey.100' }}
-      >
-        <GithubOutlined />
-      </IconButton>
+    return (
+        <>
+            {!downLG && <Search />}
+            {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
+            <IconButton
+                component={Link}
+                href="https://github.com/dsds60321/homepage"
+                target="_blank"
+                disableRipple
+                color="secondary"
+                title="GIT_HUB"
+                sx={{ color: 'text.primary', bgcolor: 'grey.100' }}
+            >
+                <GithubOutlined />
+            </IconButton>
 
-      <Notification />
-      {!downLG && <Profile />}
-      {downLG && <MobileSection />}
-    </>
-  );
+            <Notification />
+            {!downLG && <Profile />}
+            {downLG && <MobileSection />}
+        </>
+    );
 }
